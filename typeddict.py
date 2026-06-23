31dbc73073aeb84172d7a7d50cb4a45db38bba17
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2026-06-23 16:31:59 +0100

    Auto sync commit

--- typeddict.py
+++ typeddict.py
@@ -1,1 +1,16 @@
 from typing import TypedDict
+
+
+class PlayerStatus(TypedDict):
+    name: str
+    level: int
+    position: tuple[int, int]
+
+
+player: PlayerStatus = {
+    "name": "rgs",
+    "level": 3,
+    "position": (3, 0),
+}
+
+print(player["nick"])
