2c85a4cf383948500e04bd844d35fc5b87bcf309
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2026-06-23 18:25:46 +0100

    Auto sync commit

--- first_overload.py
+++ first_overload.py
@@ -1,4 +1,14 @@
-from typing import overload
+from typing import overload, reveal_type
+
+@overload
+def add(left: int, right: int) -> int: ...
+
+@overload
+def add(left: str, right: str) -> str: ...
 
 def add(left: int | str, right: int | str) -> int | str:
     return left + right
+
+
+reveal_type(add(2, 3))  # int
+reveal_type(add("oi", "bye"))  # str
