9bce5d4a294983c7bdcb964ee95f5ee21fa6f164
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2026-06-23 18:19:31 +0100

    Auto sync commit

--- player.py
+++ player.py
@@ -53,5 +53,3 @@ process_action(player, Action.MOVE, 1, 1)
 process_action(player, Action.TALK, "Hello!")
 process_action(player, Action.SLEEP)
 process_action(player, Action.LEVEL_UP)
-
-process_action(player, Action.SLEEP, "Hello!", 1, 1, True, [])
