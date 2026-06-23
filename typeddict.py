dbaab9e83dd4024381eaacbd63bffe684e01f3a5
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2026-06-23 16:45:18 +0100

    Auto sync commit

--- typeddict.py
+++ typeddict.py
@@ -1,24 +1,25 @@
-from typing import TypedDict, Required
+from typing import TypedDict, NotRequired, ReadOnly
 
 """
-class PlayerStatus(TypedDict):
-    name: str
-    level: int
-    position: tuple[int, int]
-    status_condition: NotRequired[str]
-"""
-
 class PlayerStatus(TypedDict, total=False):
     name: Required[str]
     level: Required[int]
     position: Required[tuple[int, int]]
     status_condition: str
+"""
 
+
+class PlayerStatus(TypedDict):
+    name: ReadOnly[str]  # 3.13+
+    level: int
+    position: tuple[int, int]
+    status_condition: NotRequired[str]
 
 player: PlayerStatus = {
     "name": "rgs",
     "level": 3,
     "position": (3, 0),
 }
+player["name"] = "rodrigo"
 
 print(player["position"])
